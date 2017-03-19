b4237805202f536b3f8f7cfc02f6eaef0fa199a2
xuyue <xuyue@abcdeMacBook-Pro.local>
2017-03-19 14:40:04 +0800

    init

---
+++ src/main/java/com/purplefish/netty/demo/client/SimpleClient.java
@@ -0,0 +1,7 @@
+package com.purplefish.netty.demo.client;
+
+/**
+ * Created by xuyue on 2017/3/19.
+ */
+public class SimpleClient {
+}
---
+++ src/main/java/com/purplefish/netty/demo/server/SimpleServer.java
@@ -0,0 +1,7 @@
+package com.purplefish.netty.demo.server;
+
+/**
+ * Created by xuyue on 2017/3/19.
+ */
+public class SimpleServer {
+}
